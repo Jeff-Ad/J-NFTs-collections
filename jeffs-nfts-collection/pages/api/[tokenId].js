9d96c4486cbae8b30fa4981eb212a00e1e76eaca
jeff-email <adakolejeffrey@gmail.com>
2022-09-19 16:57:53 +0100

    fx

--- jeffs-nfts-collection/pages/api/[tokenId].js
+++ jeffs-nfts-collection/pages/api/[tokenId].js
@@ -5,17 +5,17 @@
 // Token ID = 1
 // tokenURI(1) => https://example.com/
 
-export default function handler(req, res) {
-  const tokenId = req.query.tokenId;
+// export default function handler(req, res) {
+//   const tokenId = req.query.tokenId;
 
-  const name = `Crypto Devs #${tokenId}`;
-  const description = "CryptoDevs is an NFT Collection for Web3 Developers";
-  const image = `https://raw.githubusercontent.com/LearnWeb3DAO/NFT-Collection/main/my-app/public/cryptodevs/${+(
-    tokenId - 1
-  )}.svg`;
-  return res.json({
-    name: name,
-    description: description,
-    image: image,
-  });
-}
+//   const name = `Crypto Devs #${tokenId}`;
+//   const description = "CryptoDevs is an NFT Collection for Web3 Developers";
+//   const image = `https://raw.githubusercontent.com/LearnWeb3DAO/NFT-Collection/main/my-app/public/cryptodevs/${+(
+//     tokenId - 1
+//   )}.svg`;
+//   return res.json({
+//     name: name,
+//     description: description,
+//     image: image,
+//   });
+// }
